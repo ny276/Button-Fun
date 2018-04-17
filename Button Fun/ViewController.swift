a3495ad2c5caa7e51a7001cb94e63ee89bbfe753
D7703_03 <d7703_03@D7703-03ui-iMac.local>
2018-04-17 13:48:09 +0900

    add label, button

--- Button Fun/ViewController.swift
+++ Button Fun/ViewController.swift
@@ -9,17 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var count = 0
+    @IBOutlet weak var lblResult: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.black
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func btPressed(_ sender: Any) {
+        count = count + 1
+        lblResult.text = "\(count) button pressed!!"
     }
-
-
 }
 
